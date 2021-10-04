2a45d91950af570f2972e7d05667bd3d091d2542
William Drew <wdrew@governmentcio.com>
2021-10-04 12:35:47 -0400

    Added builds for amd64 and arm64 platforms

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build --no-cache -t williamdrew/rpi-demo .
+#docker build --no-cache -t williamdrew/rpi-demo .
+
+docker buildx build --platform linux/amd64,linux/arm64 -t williamdrew/rpi-demo:latest --push .
