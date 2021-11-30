52c4eaf2b9c52a84e63d2bdb2a1a8dc14664ac71
William Drew <wdrew@governmentcio.com>
2021-11-30 14:12:30 -0500

    Added high PriorityClass object

--- deploy-application.sh
+++ deploy-application.sh
@@ -2,5 +2,7 @@
 
 kubectl apply -f deployment.yaml
 
-kubectl rollout status deployment/rpi-demo-app -n rpi
+kubectl get deployment rpi-demo-app -o yaml | linkerd inject - | kubectl apply -f -
+
+#kubectl rollout status deployment/rpi-demo-app -n rpi
 
