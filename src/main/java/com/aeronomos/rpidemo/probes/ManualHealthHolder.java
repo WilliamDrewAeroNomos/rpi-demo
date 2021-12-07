e6292ab8268aac65c392dad89c7527ea7035caf0
William Drew <wdrew@governmentcio.com>
2021-12-07 07:36:41 -0500

    Removed f*g GCIO reference

--- src/main/java/com/aeronomos/rpidemo/probes/ManualHealthHolder.java
+++ src/main/java/com/aeronomos/rpidemo/probes/ManualHealthHolder.java
@@ -8,7 +8,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.springframework.stereotype.Component;
 
 /**
- * @author <a href=mailto:support@governmentcio.com>support
  *
  */
 @Component
